7b197c3346b2673f1ee143e16df3a01ebc1e8f25
nikola <djordjevic.nikola.it@gmail.com>
2025-06-24 17:53:38 +0200

    Added new line

--- src/noviPaket/Student2.java
+++ src/noviPaket/Student2.java
@@ -3,5 +3,6 @@ package noviPaket;
 public class Student2{
     public static void main(String[] args) {
         System.out.println("Nova promena");
+        System.out.println("Druga promena");
     }
 }

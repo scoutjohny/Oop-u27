e8ff01f81a7d536889c9f07d9a1a09835f0e759d
Nikola Djordjevic <djordjevic.nikola.it@gmail.com>
2025-06-24 17:55:36 +0200

    Update Student2.java
    Remote commit

--- src/noviPaket/Student2.java
+++ src/noviPaket/Student2.java
@@ -4,5 +4,6 @@ public class Student2{
     public static void main(String[] args) {
         System.out.println("Nova promena");
         System.out.println("Druga promena");
+        System.out.println("Treca promena");
     }
 }
